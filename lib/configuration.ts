9bc1fb816c0ce239727c139180d2564e6452bbce
abdullah5abid <abdullah@webilogics.com>
2023-06-08 16:45:39 +0500

    Updated Build code

--- lib/configuration.ts
+++ lib/configuration.ts
@@ -1,6 +1,7 @@
 export const CARMATECH_CONFIG = {
     Prod: {
         ARN: 'arn:aws:codestar-connections:ap-northeast-1:395929101814:connection/35fdb94f-4288-4780-8486-1f9dfc55b0b8',
+        SECRET_ARN: 'arn:aws:secretsmanager:ap-northeast-1:395929101814:secret:github-token-SsHTpI',
         ACCOUNT_ID: '395929101814',
         REGION: 'ap-northeast-1',
     }
--- lib/lambda-pipeline-stack.ts
+++ lib/lambda-pipeline-stack.ts
@@ -1,96 +1,104 @@
-import { Stack, StackProps, Stage } from "aws-cdk-lib";
+import { SecretValue, Stack, StackProps, Stage } from "aws-cdk-lib";
 import { Artifact, Pipeline } from "aws-cdk-lib/aws-codepipeline";
-import { CloudFormationCreateUpdateStackAction, CodeBuildAction, CodeStarConnectionsSourceAction } from "aws-cdk-lib/aws-codepipeline-actions";
+import { CloudFormationCreateUpdateStackAction, CodeBuildAction, CodeStarConnectionsSourceAction, GitHubSourceAction, GitHubTrigger } from "aws-cdk-lib/aws-codepipeline-actions";
 import { Construct } from "constructs";
 import { CARMATECH_CONFIG } from "./configuration";
 import { BuildSpec, LinuxBuildImage, PipelineProject } from "aws-cdk-lib/aws-codebuild";
 
 export class DailyFoodNotificationLambdaPipeline extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-      super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-      // Artifact for the source output
-      const sourceOutput = new Artifact();
+        // Artifacts
+        const sourceOutput = new Artifact();
+        const buildOutput = new Artifact('BuildOutput');
 
-      // The source action represents the retrieval of the lambda code from the Carma-tech/internal repo
-      const sourceAction = new CodeStarConnectionsSourceAction({
-          actionName: 'Lambda_Source',
-          connectionArn: CARMATECH_CONFIG.Prod.ARN,
-          owner: 'abdullah5abid',
-          repo: 'misc_cdk',
-          output: sourceOutput,
-          branch: 'master', 
-      });
+        // Source Action
+        const sourceAction = new GitHubSourceAction({
+            actionName: 'Lambda_Source',
+            owner: 'abdullah5abid',
+            repo: 'misc_cdk',
+            output: sourceOutput,
+            branch: 'master',
+            oauthToken: SecretValue.secretsManager('github-token'),
+            trigger: GitHubTrigger.WEBHOOK,
+        });
 
-      // CodeBuild project
-      const buildProject = new PipelineProject(this, 'LambdaBuild', {
-          buildSpec: BuildSpec.fromObject({
-              version: '0.2',
-              phases: {
-                  install: {
-                      'runtime-versions': {
-                          python: 3.9
-                      },
-                      commands: [
-                          'cd MISC/',
-                          'pip install -r requirements.txt -t .'
-                      ]
-                  },
-                  build: {
-                      commands: [
-                          'zip -r lambda_function.zip .'
-                      ]
-                  }
-              },
-              artifacts: {
-                  files: [
-                      'lambda_function.zip'
-                  ]
-              }
-          }),
-          environment: {
-              buildImage: LinuxBuildImage.STANDARD_5_0,
-          },
-      });
+        // Build Project
+        const buildProject = new PipelineProject(this, 'LambdaBuild', {
+            buildSpec: BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    install: {
+                        'runtime-versions': {
+                            python: 3.9
+                        },
+                        commands: [
+                            'cd MISC/',
+                            'pip install -r requirements.txt -t .',
+                            'cd ..', // go back to project root
+                            'npm install -g aws-cdk', // install CDK
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            'cd misc_cdk',
+                            'npm install', // Install dependencies for the TypeScript CDK app
+                            'npm run build', // Build the TypeScript CDK app
+                            'cdk synth -o dist', // Synthesize the app into CloudFormation template
+                            'cd ..',
+                            'cd MISC/', // Change into the directory containing the Python lambda function
+                            'zip -r ../misc_cdk/dist/lambda_function.zip .',
+                        ]
+                    }
+                },
+                artifacts: {
+                    files: [
+                        'misc_cdk/dist/**/*'
+                    ]
+                },
+                cache: {
+                    paths: ['misc_cdk/node_modules/**/*']
+                }
+            }),
+            environment: {
+                buildImage: LinuxBuildImage.STANDARD_5_0,
+            },
+        });
 
-      // Build stage
-      const buildAction = new CodeBuildAction({
-          actionName: 'Lambda_Build',
-          project: buildProject,
-          input: sourceOutput,
-          outputs: [new Artifact('BuildOutput')],
-      });
+        // Build Action
+        const buildAction = new CodeBuildAction({
+            actionName: 'Lambda_Build',
+            project: buildProject,
+            input: sourceOutput,
+            outputs: [buildOutput],
+        });
 
-      const buildOutput = new Artifact('BuildOutput');
-      // Define the action that deploys the CloudFormation template
-      const deployAction = new CloudFormationCreateUpdateStackAction({
-          actionName: 'Lambda_Deploy',
-          templatePath: buildOutput.atPath('template.json'),
-          stackName: 'CarmaTechLambdaStack',
-          adminPermissions: true,
-      });
+        // Deploy Action
+        const deployAction = new CloudFormationCreateUpdateStackAction({
+            actionName: 'Lambda_Deploy',
+            templatePath: buildOutput.atPath('misc_cdk/dist/MiscCdkStack.template.json'),
+            stackName: 'CarmaTechLambdaStack',
+            adminPermissions: true,
+        });
 
-
-      // Deploy stage
-      const deployStage = new Stage(this, 'Deploy');
-
-      // Create pipeline
-      const pipeline = new Pipeline(this, 'DailyFoodNotificationLambdaPipeline', {
-          pipelineName: 'DailyFoodNotificationLambdaPipeline',
-          stages: [
-              {
-                  stageName: 'Source',
-                  actions: [sourceAction],
-              },
-              {
-                  stageName: 'Build',
-                  actions: [buildAction],
-              },
-              {
-                  stageName: 'Deploy',
-                  actions: [deployAction], 
-              },
-          ],
-      });
-  }
+        // Pipeline
+        const pipeline = new Pipeline(this, 'DailyFoodNotificationLambdaPipeline', {
+            pipelineName: 'DailyFoodNotificationLambdaPipeline',
+            stages: [
+                {
+                    stageName: 'Source',
+                    actions: [sourceAction],
+                },
+                {
+                    stageName: 'Build',
+                    actions: [buildAction],
+                },
+                {
+                    stageName: 'Deploy',
+                    actions: [deployAction],
+                },
+            ],
+        });
+    }
 }
